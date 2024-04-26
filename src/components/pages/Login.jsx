a5a1e115bf72af8454c8c0bed6ba724c54b68e51
Farzaam Ali <farzaamali004@gmail.com>
2024-04-26 11:03:07 +0500

    update forms

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -5,21 +5,34 @@ function Login() {
   const onSubmit = (data) => console.log(data)
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)} action="#">
-      <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
-        </div>
+      <div className=' p-10 m=auto '>
 
-        <div class="mb-6">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-          <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-        </div> 
+        <form onSubmit={handleSubmit(onSubmit)} action="#">
+          <div className=''>
+            <label for="first_name" className=" block mb-2 text-sm font-medium">E-mail</label>
+            <input type="text" id="first_name" className=" mb-4 bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="abc@email.com" required {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: 'Invalid email address',
+              },
+            })} />
+          </div>
 
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          <div class="mb-6">
+            <label for="password" className="block mb-2 text-sm font-medium">Password</label>
+            <input type="password" id="password" class="border  text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="•••••••••" required  {...register("password", {
+              required: true
+            })} />
+          </div>
 
-    </form>
-    
+          <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 ">Submit</button>
+
+        </form>
+      </div>
+
+
+
     </>
   )
 }
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -2,33 +2,49 @@ import React from 'react'
 import { useForm } from "react-hook-form"
 function Signup() {
 
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, watch } = useForm()
   const onSubmit = (data) => console.log(data)
 
 
   return (
     <>
-     <form onSubmit={handleSubmit(onSubmit)} action="#">
-      <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" 
-            {...register("email", { required: true})}/>
-        </div>
+      <div className=' p-10 m=auto '>
 
-        <div class="mb-6">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-          <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••"
-          />
-        </div> 
+        <form onSubmit={handleSubmit(onSubmit)} action="#">
+          <div className='mb-6'>
+            <label for="first_name" className=" block mb-2 text-sm font-medium">E-mail</label>
+            <input type="text" id="first_name" className=" mb-4 bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="abc@email.com" required  {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: 'Invalid email address',
+              },
+            })} />
+          </div>
 
-        <div class="mb-6">
-        <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-        <input type="password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+          <div class="mb-6">
+            <label for="password" className="block mb-2 text-sm font-medium">Password</label>
+            <input type="password" id="password" required  {...register("password", {
+              required: true
+            })}
+              class="border  text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="•••••••••" />
+          </div>
+          <div class="mb-6">
+            <label for="confirm_password" className="block mb-2 text-sm font-medium">Confirm Password</label>
+            <input type="password" id="confirm_password" required  {...register("confirm_password", {
+              required: true,
+              validate: (val) => {
+                if (watch('password') != val) {
+                  console.log("Your passwords do no match");
+                }
+              },
+            })} class="border  text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="•••••••••" />
+          </div>
+
+          <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 ">Submit</button>
+
+        </form>
       </div>
-      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-
-
-    </form>
     </>
   )
 }
