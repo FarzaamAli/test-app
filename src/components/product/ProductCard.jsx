cea62bf2802188456eddc5d93b18a26abf352f73
Farzaam Ali <farzaamali004@gmail.com>
2024-04-26 16:01:24 +0500

    product card update

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -5,11 +5,13 @@ function ProductCard(props) {
   const { name, price, pid } = props
 
   return (
-    <div>
-            <h2>{name}</h2>
-            <p>{price}</p>
-            <Link to={`/product/${pid}`}>view</Link>
-        </div>
+    <div className='flex m-4'>
+      <div className='border-double border-4 border-sky-500 p-4 '>
+        <h2 className=' font-bold text-center'>{name}</h2>
+        <p className=' p-4'>Price: ${price}</p>
+        <button className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2 "><Link to={`/product/${pid}`}>view</Link></button>
+      </div>
+    </div>
   )
 }
 
